640d417380e3a05a99a9b73de6bd83e92c390c0d
cegarciav <cegarcia2@uc.cl>
2021-06-13 20:18:24 -0400

    Add mailer service to the app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const appLogger = require('./logs');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,18 @@ routes(app);
 // Make appLogger available for the whole app
 app.locals.logger = new appLogger('development');
 
+// Make email account and email service available for the whole app
+async function startEmailSender() {
+    app.locals.emailSender = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: "INSERT EMAIL", // generated ethereal user
+            pass: "INSERT PASSWORD", // generated ethereal password
+        },
+    });
+}
+
+startEmailSender();
 
 app.listen(port, function() {
     console.log('Server started on port: ' + port);
